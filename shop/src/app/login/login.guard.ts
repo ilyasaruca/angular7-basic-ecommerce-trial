803ce24dfb5adddb76166b89915a92bfb118d8e7
ilyasaruca <ilyasaruca@gmail.com>
2020-03-19 13:51:53 +0300

    first commit
    first commit

---
+++ shop/src/app/login/login.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AccountService } from '../services/account.service';
+
+@Injectable()
+
+export class LoginGuard implements CanActivate {
+    
+    constructor(private accountService: AccountService, private router: Router) { }
+ 
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        
+        let logged = this.accountService.isLoggedIn; 
+        if(logged){   
+            return true;
+        } 
+        else{  
+            this.router.navigate(["login"]);
+            return false;
+        }
+    }
+}
---
+++ shop/src/app/services/account.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { User } from '../login/user';
+
+@Injectable()
+
+export class AccountService {
+
+  constructor() { }
+
+  loggedIn = false;
+
+  login(user:User):boolean{ 
+    if(user.userName =="ilyas" && user.password == "12345678"){
+      this.loggedIn = true;
+      localStorage.setItem("isLogged",user.userName); 
+      return true;
+    } 
+    else{
+      return false;
+    }
+      
+  }
+
+  isLoggedIn(){
+    return this.loggedIn;
+  }
+
+  logOut(){
+    localStorage.removeItem("isLogged");
+    this.loggedIn = false;
+  }
+}
